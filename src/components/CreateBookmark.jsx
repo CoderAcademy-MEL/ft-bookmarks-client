abbe30d04a37b59235acdd9153a0fec084e5daa3
Harrison Malone <harrison.malone2@gmail.com>
2020-06-30 13:48:03 +1000

    added create bookmarks component with input change and input submit events

---
+++ src/components/CreateBookmark.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styles from "../stylesheets/createBookmark.module.css";
+
+class CreateBookmark extends React.Component {
+  onInputChange = (event) => {
+    const key = event.target.id;
+    this.setState({
+      [key]: event.target.value,
+    });
+  };
+
+  onFormSubmit = async (event) => {
+    event.preventDefault();
+    const body = {
+      bookmark: this.state
+    }
+    await fetch("http://localhost:3000/bookmarks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    this.props.history.push("/bookmarks");
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <form onSubmit={this.onFormSubmit}>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={this.onInputChange}
+          />
+          <label htmlFor="url">Url</label>
+          <input
+            type="text"
+            name="url"
+            id="url"
+            onChange={this.onInputChange}
+          />
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            id="description"
+            onChange={this.onInputChange}
+          ></textarea>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateBookmark;
