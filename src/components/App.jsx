df5785f281766681551e231a234b9962344044e6
Harrison Malone <harrison.malone2@gmail.com>
2020-07-01 15:36:34 +1000

    all /bookmark routes now are protected by jwt logic, also have routes for login and signup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,33 @@
-// App.jsx
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "./Home";
-import Bookmarks from "./Bookmarks";
-import NoMatch from "./NoMatch";
-import Navbar from "../shared/Navbar";
-import CreateBookmark from "./CreateBookmark";
-import EditBookmark from "./EditBookmark";
-import ProtectedRoute from "./ProtectedRoute";
-import Secrets from "./Secrets";
-
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Switch>
-          <ProtectedRoute exact path="/secrets" component={Secrets} />
-          <Route exact path="/bookmarks/:id/edit" component={EditBookmark} />
-          <Route exact path="/bookmarks/create" component={CreateBookmark} />
-          <Route exact path="/bookmarks" component={Bookmarks} />
-          <Route exact path="/" component={Home} />
-          <Route component={NoMatch} />
-        </Switch>
-      </>
-    );
-  }
-}
-
-export default App;
+    // App.jsx
+    import React from "react";
+    import { Route, Switch } from "react-router-dom";
+    import Home from "./Home";
+    import Bookmarks from "./Bookmarks";
+    import NoMatch from "./NoMatch";
+    import Navbar from "../shared/Navbar";
+    import CreateBookmark from "./CreateBookmark";
+    import EditBookmark from "./EditBookmark";
+    import ProtectedRoute from "./ProtectedRoute";
+    import Login from './Login'
+    import SignUp from './SignUp'
+    
+    class App extends React.Component {
+      render() {
+        return (
+          <>
+            <Navbar />
+            <Switch>
+              <ProtectedRoute exact path="/bookmarks/:id/edit" component={EditBookmark} />
+              <ProtectedRoute exact path="/bookmarks/create" component={CreateBookmark} />
+              <ProtectedRoute exact path="/bookmarks" component={Bookmarks} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/sign-up" component={SignUp} />
+              <Route exact path="/" component={Home} />
+              <Route component={NoMatch} />
+            </Switch>
+          </>
+        );
+      }
+    }
+    
+    export default App;
