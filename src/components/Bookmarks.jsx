6856e2cf5de8c0468761de3d56eea5199f94d723
Harrison Malone <harrison.malone2@gmail.com>
2020-07-03 13:56:33 +1000

    no state in bookmarks, state comes from context, getBookmarks now not needed, bookmarks populated in protected route, renderBookmarks maps through context, delete bookmark method dispatches to bookmark and we don't need to await the http response

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -1,33 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import moment from 'moment'
+import { BookmarksContext } from '../store/bookmarks-context'
 
 class Bookmarks extends React.Component {
-  // initial state
-  state = { bookmarks: [] };
-
-  getBookmarks = async () => {
-    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookmarks`, {
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    });
-    const bookmarks = await response.json();
-    this.setState({ bookmarks: bookmarks });
-  };
+  static contextType = BookmarksContext;
 
   deleteBookmark = async (id) => {
-    await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookmarks/${id}`, {
+    this.context.dispatch("delete", id)
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/bookmarks/${id}`, {
       method: "DELETE",
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
       }
     });
-    this.getBookmarks();
   };
 
   renderBookmarks = () => {
-    return this.state.bookmarks.map((bookmark, index) => {
+    return this.context.bookmarks.map((bookmark, index) => {
       return (
         <div key={index} className="bookmark">
           <h3>{bookmark.title}</h3>
@@ -37,7 +27,7 @@ class Bookmarks extends React.Component {
               {bookmark.url}
             </a>
           </span>
-          <p>{moment(bookmark.created_at).startOf('minute').fromNow()}</p>
+          <p>{moment(bookmark.updated_at).startOf('minute').fromNow()}</p>
           <div className="edit-delete-container">
             <Link to={`/bookmarks/${bookmark.id}/edit`}>Edit</Link>
             <span onClick={() => this.deleteBookmark(bookmark.id)}>Delete</span>
@@ -47,10 +37,6 @@ class Bookmarks extends React.Component {
       );
     });
   };
-
-  async componentDidMount() {
-    this.getBookmarks();
-  }
 
   render() {
     return <div>{this.renderBookmarks()}</div>;
