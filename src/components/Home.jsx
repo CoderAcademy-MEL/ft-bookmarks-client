484fea94818048d6a7a75cdc521a765bfb7734a3
Harrison Malone <harrison.malone2@gmail.com>
2020-07-03 14:01:44 +1000

    home component reverted back to basic h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,9 @@
 // Home.jsx
 import React from "react";
-import { RandomContext } from '../store/random-context'
 
 class Home extends React.Component {
   render() {
-    return (
-      <RandomContext.Consumer>
-        {context => {
-          console.log(context.value)
-          return (
-            <h1 onClick={context.updateValue}>home</h1>
-          )
-        }}
-      </RandomContext.Consumer> 
-    ) 
+    return <h1>home</h1>;
   }
 }
 
