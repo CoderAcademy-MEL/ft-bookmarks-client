c3c9540c301fffc9f113146a1cfcc0ef72f9075f
Harrison Malone <harrison.malone2@gmail.com>
2020-07-03 16:02:51 +1000

    navbar now contains some conditional logic for when user is authenticated or not

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -10,19 +10,26 @@ const NavBar = () => {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/bookmarks">Bookmarks</Link>
-      <Link to="/bookmarks/create">Create Bookmark</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/sign-up">Sign Up</Link>
-      <span
-        onClick={() => {
-          localStorage.removeItem("token");
-          context.dispatch("logout")
-          history.push("/login");
-        }}
-      >
-        Logout
-      </span>
+      {context.auth ? (
+        <>
+          <Link to="/bookmarks">Bookmarks</Link>
+          <Link to="/bookmarks/create">Create Bookmark</Link>
+          <span
+            onClick={() => {
+              localStorage.removeItem("token");
+              context.dispatch("logout")
+              history.push("/login");
+            }}
+          >
+            Logout
+          </span>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/sign-up">Sign Up</Link>
+        </>
+      )}
     </nav>
   );
 };
