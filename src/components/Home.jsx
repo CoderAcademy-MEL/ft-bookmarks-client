963b1eacd36436f3ee9d460885bafa7ff35638ed
Harrison Malone <harrison.malone2@gmail.com>
2020-07-09 23:42:51 +1000

    added lots of markup for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,36 @@
-// Home.jsx
 import React from "react";
 
-const Home = () => (
-  <div className="home">
-    <p><a href="/">HarrisonsBookmarks</a> is a demo app that I completed in my demo code along classes for <a href="https://coderacademy.edu.au/">Coder Academy</a> Fast Track 2020-1.</p>
-    <p>It uses a React client with a standard setup using a <a href="https://reactrouter.com/web/guides/quick-start">react-router-dom</a> for routing and <a href="https://momentjs.com/">moment.js</a> for dealing with dates.</p>
-    <p>It uses a Rails backend that uses <a href="https://github.com/nsarno/knock">knock</a> for handling authentication.</p>
-    <p>I've also tried to ensure the CSS makes the site mobile friendly as well as using <a href="https://sass-lang.com/">SASS</a> for preprocessing.</p>
-  </div>
-)
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <p>
+          <a href="/">HarrisonsBookmarks</a> is a demo app that I completed in
+          my demo code along classes for{" "}
+          <a href="https://coderacademy.edu.au/">Coder Academy</a> Fast Track
+          2020-1.
+        </p>
+        <p>
+          It uses a React client with a standard setup using a{" "}
+          <a href="https://reactrouter.com/web/guides/quick-start">
+            react-router-dom
+          </a>{" "}
+          for routing and <a href="https://momentjs.com/">moment.js</a> for
+          dealing with dates.
+        </p>
+        <p>
+          It uses a Rails backend that uses{" "}
+          <a href="https://github.com/nsarno/knock">knock</a> for handling
+          authentication.
+        </p>
+        <p>
+          I've also tried to ensure the CSS makes the site mobile friendly as
+          well as using <a href="https://sass-lang.com/">SASS</a> for
+          preprocessing.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Home;
