17419dd209e9121388f4735f9b0acad3acd150e4
Harrison Malone <harrison.malone2@gmail.com>
2020-07-09 23:45:51 +1000

    home link changed

--- src/shared/LoggedOutNavbar.jsx
+++ src/shared/LoggedOutNavbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const LoggedOutNavbar = () => (
   <div className="logged-out-links">
-    <Link to="/">Home</Link>
+    <Link to="/"><span role="img" aria-label="site logo">🔗</span></Link>
     <Link to="/login">Login</Link>
     <Link to="/sign-up">Sign Up</Link>
   </div>
