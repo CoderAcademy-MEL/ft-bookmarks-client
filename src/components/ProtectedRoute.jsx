9a8c8c3ba451496e61f0cfe50be78e3118013576
Harrison Malone <harrison.malone2@gmail.com>
2020-07-09 11:51:35 +1000

    where the bulk of the changes occured: http request goes to /bookmarks and responds with jwt and bookmarks, then dispatch, various functions in here all invoked in componentDidMount

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import { BookmarksContext } from '../context/bookmarks-context'
 
 class ProtectedRoute extends React.Component {
+  static contextType = BookmarksContext
   state = {
     auth: false,
     loading: true,
   };
 
+  getBookmarks = async () => {
+    return await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/bookmarks`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+  }
+
+  checkStatusCode = (response) => {
+    if (response.status >= 400) {
+      throw new Error("not authorized");
+    }
+  }
+
+  setTokenAndDispatch = async (response) => {
+    const { jwt, bookmarks, current_user: currentUser } = await response.json() 
+    localStorage.setItem("token", jwt);
+    this.context.dispatch("populate", { bookmarks, currentUser })
+  }
+
+  setAuth = () => this.setState({ auth: true, loading: false })
+
+  setLoading = () => this.setLoading({ loading: false })
+
   async componentDidMount() {
     try {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/status`, {
-        headers: {
-          'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
-      })
-      if (response.status >= 400) {
-        throw(new Error("not authorized"))
-      } else { 
-        const { jwt } = await response.json()        
-        localStorage.setItem('token', jwt)
-        this.setState({
-          auth: true,
-          loading: false,
-        });
-      }
+      const response = await this.getBookmarks()
+      this.checkStatusCode(response)
+      await this.setTokenAndDispatch(response)
+      this.setAuth()
     } catch(err) {
-      this.setState({
-        loading: false,
-      });
+      this.setLoading()
     }
   }
 
