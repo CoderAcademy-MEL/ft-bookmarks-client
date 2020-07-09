4ca73339fba55cf38f2cc34490854d3740282f82
Harrison Malone <harrison.malone2@gmail.com>
2020-07-09 23:45:29 +1000

    logged in navbar now has seperate event handler for logout

--- src/shared/LoggedInNavbar.jsx
+++ src/shared/LoggedInNavbar.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const handleLogout = (props) => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("auth");
+  props.context.dispatch("logout");
+  props.history.push("/");
+};
 
 const LoggedInNavbar = (props) => (
   <>
     <div className="logged-in-links">
+      <Link to="/">
+        <span role="img" aria-label="site logo">
+          🔗
+        </span>
+      </Link>
       <Link to="/bookmarks">Bookmarks</Link>
       <Link to="/bookmarks/create">Create Bookmark</Link>
-      <span
-        id="logout"
-        onClick={() => {
-          localStorage.removeItem("token");
-          props.context.dispatch("logout")
-          props.history.push("/");
-        }}
-      >
-        Logout
-      </span>
     </div>
     <div className="welcome-user">
-      <span>{props.context.currentUser}</span>
+      <span id="logout" onClick={() => handleLogout(props)}>
+        Logout
+      </span>
+      <Link to="/profile">Profile</Link>
     </div>
   </>
-)
+);
 
-export default LoggedInNavbar
+export default LoggedInNavbar;
