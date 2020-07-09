cda513677889b5ebc66dbb63a15f32a77a758cc0
Harrison Malone <harrison.malone2@gmail.com>
2020-07-09 23:47:27 +1000

    more cleaning up of protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,49 +1,48 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { BookmarksContext } from '../context/bookmarks-context'
+import { BookmarksContext } from "../context/bookmarks-context";
 
 class ProtectedRoute extends React.Component {
-  static contextType = BookmarksContext
+  static contextType = BookmarksContext;
+
   state = {
     auth: false,
     loading: true,
   };
 
   getBookmarks = async () => {
-    return await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/bookmarks`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      }
-    );
-  }
+    return await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookmarks`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+  };
 
   checkStatusCode = (response) => {
     if (response.status >= 400) {
       throw new Error("not authorized");
     }
-  }
+  };
 
   setTokenAndDispatch = async (response) => {
-    const { jwt, bookmarks, current_user: currentUser } = await response.json() 
+    const { jwt, bookmarks, current_user: currentUser } = await response.json();
     localStorage.setItem("token", jwt);
-    this.context.dispatch("populate", { bookmarks, currentUser })
-  }
+    localStorage.setItem("auth", true);
+    this.context.dispatch("populate", { bookmarks, currentUser });
+  };
 
-  setAuth = () => this.setState({ auth: true, loading: false })
+  setAuth = () => this.setState({ auth: true, loading: false });
 
-  setLoading = () => this.setLoading({ loading: false })
+  setLoading = () => this.setState({ loading: false });
 
   async componentDidMount() {
     try {
-      const response = await this.getBookmarks()
-      this.checkStatusCode(response)
-      await this.setTokenAndDispatch(response)
-      this.setAuth()
-    } catch(err) {
-      this.setLoading()
+      const response = await this.getBookmarks();
+      this.checkStatusCode(response);
+      await this.setTokenAndDispatch(response);
+      this.setAuth();
+    } catch (err) {
+      this.setLoading();
     }
   }
 
