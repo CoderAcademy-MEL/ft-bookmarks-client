8dc6924ad21fcb37c590c095c2d3c4a193032c43
Harrison Malone <harrison.malone2@gmail.com>
2020-07-17 15:58:42 +1000

    forgot to save conflict file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-import '@testing-library/cypress/add-commands';
-=======
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('getUser', () => {
@@ -54,4 +25,3 @@ Cypress.Commands.add("login", (email, password) => {
     localStorage.setItem("token", response.body.jwt)
   })
 })
->>>>>>> cypress-implementation-2
