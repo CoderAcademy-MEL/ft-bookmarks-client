f34edadd5032b11a31254421f2bb68db1efd0cf4
Harrison Malone <harrison.malone2@gmail.com>
2020-07-17 16:26:05 +1000

    added testid to sign up and using login credientials helper

--- cypress/e2e/register.spec.js
+++ cypress/e2e/register.spec.js
@@ -4,12 +4,10 @@ describe("signing up as user", () => {
   it("should register as a new user", () => {
     const user = userBuilder();
     cy.visit("/");
-    cy.findByText(/Sign Up/i).click();
-    cy.findByLabelText(/Email/).type(user.email);
-    cy.findByLabelText(/Password/).type(user.password);
-    cy.findByText(/Submit/).click();
+    cy.findByTestId("sign-up").click();
+    cy.typeInLoginCredentials(user.email, user.password)
+    cy.get('form').submit()
     cy.url().should("eq", "http://localhost:8080/bookmarks");
-    cy.window().its("localStorage.token").should("be.a", "string");
-    cy.findByTestId("no-bookmarks", {timeout: 500});
+    cy.findByTestId("no-bookmarks", {timeout: 500}).should("exist")
   });
 });
--- src/shared/LoggedOutNavbar.jsx
+++ src/shared/LoggedOutNavbar.jsx
@@ -5,7 +5,7 @@ const LoggedOutNavbar = () => (
   <div className="logged-out-links">
     <Link to="/"><span role="img" aria-label="site logo">🔗</span></Link>
     <Link to="/login" data-testid="login">Sign In</Link>
-    <Link to="/sign-up">Sign Up</Link>
+    <Link to="/sign-up" data-testid="sign-up">Sign Up</Link>
   </div>
 )
 
