c21028a788c0b2307cb1900351b2b2176cd61260
Harrison Malone <harrison.malone2@gmail.com>
2020-07-20 13:17:42 +1000

    on input change handles file inputs, on form submit creates a form data object that is sent in the body, the response is then deconstructed and bookmark value is dispatched to context, form file input implemented

--- src/components/CreateBookmark.jsx
+++ src/components/CreateBookmark.jsx
@@ -1,30 +1,42 @@
 import React from "react";
-import { BookmarksContext } from '../context/bookmarks-context'
+import { BookmarksContext } from "../context/bookmarks-context";
 
 class CreateBookmark extends React.Component {
-  static contextType = BookmarksContext
+  static contextType = BookmarksContext;
   onInputChange = (event) => {
     const key = event.target.id;
-    this.setState({
-      [key]: event.target.value,
-    });
+    if (event.target?.files) {
+      this.setState({
+        [key]: event.target.files[0]
+      })
+    } else {
+      this.setState({
+        [key]: event.target.value,
+      });
+    }
   };
 
   onFormSubmit = async (event) => {
     event.preventDefault();
-    const body = {
-      bookmark: this.state
+    // const body = {
+    //   bookmark: this.state,
+    // };
+    var data = new FormData()
+    for (let key in this.state) {
+      data.append(`bookmark[${key}]`, this.state[key])
     }
-    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookmarks`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      },
-      body: JSON.stringify(body),
-    });
-    const newBookmark = await response.json()
-    this.context.dispatch("add", newBookmark) 
+    const response = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/bookmarks`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: data,
+      }
+    );
+    const { image, bookmark } = await response.json();
+    this.context.dispatch("add", {...bookmark, image});
     this.props.history.push("/bookmarks");
   };
 
@@ -32,7 +44,7 @@ class CreateBookmark extends React.Component {
     return (
       <>
         <h1>Create a bookmark</h1>
-        <form onSubmit={this.onFormSubmit}>
+        <form onSubmit={this.onFormSubmit} encType="multipart/form-data">
           <label htmlFor="title">Title</label>
           <input
             type="text"
@@ -53,6 +65,13 @@ class CreateBookmark extends React.Component {
             id="description"
             onChange={this.onInputChange}
           ></textarea>
+          <label htmlFor="image">Image</label>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            onChange={this.onInputChange}
+          />
           <input type="submit" value="Submit" />
         </form>
       </>
